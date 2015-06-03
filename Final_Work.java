cf3a61a5001d4d80e282d03b2f801a0975b1038c
henry1758f <henry1758f@gmail.com>
2015-06-03 21:34:51 +0800

    最原始的無內容.java檔案
    我試著新增.java檔在Git上

---
+++ Final_Work.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author 
+ *
+ */
+public class Final_Work {
+
+	/**
+	 * 
+	 */
+	public Final_Work() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
