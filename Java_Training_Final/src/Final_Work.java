c3237494132fcbd94a509e33ff20852d99cbf484
henry1758f <henry1758f@gmail.com>
2015-06-16 00:32:55 +0800

    Update Final_Work.java
    加入錯誤訊息視窗
    檔案修改狀態判斷
    檔名不是.txt提醒與自動加入功能

--- Java_Training_Final/src/Final_Work.java
+++ Java_Training_Final/src/Final_Work.java
@@ -1,6 +1,8 @@
 /*文件編碼請選擇UTF-8*/
 package Final_Work;
+import javax.net.ssl.SSLEngineResult.Status;
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 
 import com.sun.glass.events.WindowEvent;
 import com.sun.javafx.binding.SelectBinding.AsString;
@@ -20,7 +22,8 @@ import java.io.ObjectOutputStream;
 
 public class Final_Work  extends JFrame implements ActionListener
 {
-	
+	boolean oldfile = false;
+	Frame frame = new Frame();
 	private static AsString Output [];
 	public static String str= new String();
 	public static char opench;
@@ -28,6 +31,9 @@ public class Final_Work  extends JFrame implements ActionListener
 	public static String Filename;
 	public static int W_width = 700;	//視窗寬
 	public static int W_height = 700;	//視窗高
+	public static String First_load;
+	private Timer change_Timer;
+	public JLabel status;
 	public Final_Work() 
 	{
 		super ("記事本");
@@ -45,7 +51,8 @@ public class Final_Work  extends JFrame implements ActionListener
 			@Override
             public void mouseReleased(MouseEvent ev)
             {
-                textArea.setText("");  //清除
+                change_Timer.start();
+				textArea.setText("");  //清除
             }
 			 
 		});
@@ -57,6 +64,7 @@ public class Final_Work  extends JFrame implements ActionListener
             public void mouseReleased(MouseEvent ev)
             {
                 fgopenFile();
+                change_Timer.start();
             }
 		});
 		JMenuItem item;
@@ -65,15 +73,42 @@ public class Final_Work  extends JFrame implements ActionListener
 			@Override
             public void mouseReleased(MouseEvent ev)
             {
-				try {
-					fgsaveFile();
-					} 
-				catch (IOException e) {e.printStackTrace();
-			}
+				try 
+				{
+					if(oldfile == false)
+					{
+						fgsaveFile();
+					}
+					else
+					{
+						//TODO 直接存檔功能
+					}
+					change_Timer.start();
+				} 
+				catch (IOException e) 
+				{
+					JOptionPane.showConfirmDialog(frame, "注意!檔案未儲存!\n存檔時發生錯誤，請再試一次!", "錯誤", JOptionPane.ERROR_MESSAGE ,0);
+					e.printStackTrace();
+				}
             }
 		});
 		file.add(item = new JMenuItem("另存新檔(A)",KeyEvent.VK_A));
-		item.addActionListener(this);
+		item.addMouseListener(new MouseAdapter() {
+			@Override
+            public void mouseReleased(MouseEvent ev)
+            {
+				try 
+				{
+					fgsaveFile();
+					change_Timer.start();
+				} 
+				catch (IOException e) 
+				{
+					JOptionPane.showConfirmDialog(frame, "注意!檔案未儲存!\n存檔時發生錯誤，請再試一次!", "錯誤", JOptionPane.ERROR_MESSAGE ,0);
+					e.printStackTrace();
+				}
+            }
+		});
 		file.addSeparator();
 		file.add(item = new JMenuItem("版面設定(U)",KeyEvent.VK_U));
 		item.addActionListener(this);
@@ -82,8 +117,19 @@ public class Final_Work  extends JFrame implements ActionListener
 		file.addSeparator();
 		file.add(item = new JMenuItem("結束(X)",KeyEvent.VK_X));
 		item.addActionListener(new ActionListener() {              //右上角關閉事件
-					public void actionPerformed(ActionEvent e) { 
-					closeFile(); 	
+					public void actionPerformed(ActionEvent e) 
+					{ 
+						if(First_load.equals(textArea.getText()))
+						{
+							change_Timer.stop();
+							closeFile(); 
+						}
+						else
+						{
+				            System.out.println(First_load + " & " + textArea.getText());  
+
+						}
+								
 					}});
 		upon.add(file);
 		//編輯
@@ -211,13 +257,38 @@ public class Final_Work  extends JFrame implements ActionListener
 	    contentPane.add(panel, BorderLayout.CENTER); 
 	      
 	    /*狀態列*/
-	    JLabel stateBar = new JLabel("未修改"); 
-	    stateBar.setHorizontalAlignment(SwingConstants.LEFT); //設定水平方向的對齊
-	    stateBar.setBorder( 
+	    JLabel status = new JLabel("未修改"); 
+	    status.setHorizontalAlignment(SwingConstants.LEFT); //設定水平方向的對齊
+	    status.setBorder( 
 	    BorderFactory.createEtchedBorder()); 
-	    contentPane.add(stateBar, BorderLayout.SOUTH);
+	    contentPane.add(status, BorderLayout.SOUTH);
+	    
+	    change_Timer = new Timer(100, this);
+	    change_Timer.addActionListener(new ActionListener() 
+	    {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				// TODO Auto-generated method stub
+				if(First_load.equals(textArea.getText()))
+				{
+					status.setText("已儲存");
+				}
+				else
+				{
+					status.setText("未儲存");
+				}
+				change_Timer.restart();
+			}
+		});
+	    
+	   
 
 	}
+
+
+	
 
 
 	public static void main(String[] args) throws IOException {
@@ -234,6 +305,7 @@ public class Final_Work  extends JFrame implements ActionListener
 		mainWin.setVisible(true);
 		
 		
+		
 	}
 
 	/*關閉檔案*/
@@ -242,15 +314,6 @@ public class Final_Work  extends JFrame implements ActionListener
 		 System.exit(0);
 	}
 
-	/*chooser方法開檔*/
-	private static void csopenFile() {
-		// TODO Auto-generated method stub
-		JFileChooser chooser = new JFileChooser();
-		int confirm = chooser.showOpenDialog(null);
-		if (confirm == chooser.APPROVE_OPTION){
-    	//JOptionPane.showMessageDialog(null, "Your file: " + chooser.getSelectedFile() + "\n,in " + chooser.getCurrentDirectory() + ".", "Message", JOptionPane.INFORMATION_MESSAGE);
-		}
-	}
 	/*filedialog方法開檔*/
 	private static void fgopenFile() {
 		// TODO Auto-generated method stub
@@ -262,24 +325,32 @@ public class Final_Work  extends JFrame implements ActionListener
 	        Filename=fd.getDirectory() +fd.getFile();   //getDirectory設定檔案的預設路徑, getFile設定檔案的預設檔名
 	        System.out.println("FileDialog---->"+Filename);  //顯示選擇的檔案路徑+名稱
 	        
-	        try {
+	        try 
+	        {
 	            FileReader fr=new FileReader(Filename); 
 	            BufferedReader bread=new BufferedReader(fr); //串流緩衝
 	            int ch;
 	            int a = 0;
-	            while ((ch=bread.read()) != -1) {          //BufferedReader 繼承自 Reader 類別的 read() 方法來讀取, 但此方法是一次讀取一個字元, 讀到檔尾時會傳回 -1
-	              opench=(char)ch;                     //opench=將ch強制轉成字元
-	              System.out.print(opench);   
-	              a++;
-	             if(a==1){continue;}
-	               str=str+String.valueOf(opench);   //字串=字串+強制轉字串後的opench
-	              }
-	             textArea.setText("");  //清除
-	             textArea.setText(str);           //最後顯示於TextArea 
-	             bread.close();
+	            while ((ch=bread.read()) != -1) 
+	            {          //BufferedReader 繼承自 Reader 類別的 read() 方法來讀取, 但此方法是一次讀取一個字元, 讀到檔尾時會傳回 -1
+	            	opench=(char)ch;                     //opench=將ch強制轉成字元
+	            	System.out.print(opench);   
+	            	a++;
+	            	if(a==1)
+	            		continue;
+	            	str=str+String.valueOf(opench);   //字串=字串+強制轉字串後的opench
 	            }
+	            textArea.setText("");  //清除
+	            textArea.setText(str);
+	            //最後顯示於TextArea 
+	            First_load = textArea.getText();	//複製原先內容
+	            bread.close();
+	        }
 	        
-	          catch (IOException e) {System.out.println(e);  //捕捉異常並顯示"e"
+	        catch (IOException e) 
+	        {
+	        	JOptionPane.showConfirmDialog(frame, "注意!\n開啟檔案時發生錯誤，請再試一次!", "錯誤", JOptionPane.OK_OPTION ,0);
+	        	System.out.println("error_openfile");  //捕捉異常並顯示"e"
 	    	
 	    }}
 	}
@@ -289,20 +360,50 @@ public class Final_Work  extends JFrame implements ActionListener
 	/*filedialog方法儲存檔案*/
 	private static void fgsaveFile() throws IOException  {
 		// TODO Auto-generated method stub
+		boolean s = false;
 		String stringfile; 
 		Frame frame = new Frame();
 		FileDialog fd = new FileDialog( frame,"儲存檔案", FileDialog.SAVE);   //LOAD=>整數 0 ，設定為開啟檔案的對話視窗
 	    fd.setVisible(true); 
 	    if(fd!=null){   
 	    	 String string1 = textArea.getText();    //檔案內容字串
-	         stringfile = fd.getDirectory()+fd.getFile()+".txt";       //當按路徑+檔案名稱字串   
-	        try {  
-	            BufferedWriter bwrite = new BufferedWriter(new FileWriter(stringfile));  //串流緩衝區
-	            bwrite.write(string1);   //寫入內容
-	            bwrite.close();   //關閉串流
-	            textArea.setText(textArea.getText()); //在對話框後扔保持文字顯示
-	        } catch (Exception e) {  
-	            // TODO Auto-generated catch block  
+	         stringfile = fd.getDirectory()+fd.getFile();//當按路徑+檔案名稱字串 
+	         if(!(stringfile.contains(".txt") || stringfile.contains(".TXT")))
+	         {
+	        	 int n= JOptionPane.showConfirmDialog(frame, "檔案副檔名似乎不是文字文件.txt類型，是否設為此類型?", "副檔名問題", JOptionPane.YES_NO_CANCEL_OPTION ,1);
+	        	 if(n==1)
+	        	 {
+	 	            System.out.println("n=1");  
+	        	 }
+	        	 else if(n==2)
+	        	 {
+		        	 JOptionPane.showConfirmDialog(frame, "注意!\n檔案未儲存!", "副檔名問題", JOptionPane.OK_OPTION ,2);
+	        		 System.out.println("n=3");
+	        		 s = true;
+	        	 }
+	        	 else
+	        	 {
+	 	            System.out.println("n=0");  
+	 	            stringfile = stringfile + ".txt";
+	        	 }
+	        	 
+	         }
+	        try 
+	        {  
+	        	if(s ==false)
+	        	{
+	        		BufferedWriter bwrite = new BufferedWriter(new FileWriter(stringfile));  //串流緩衝區
+	        		bwrite.write(string1);   //寫入內容
+	        		bwrite.close();   //關閉串流
+	        		textArea.setText(textArea.getText()); //在對話框後扔保持文字顯示
+	        		First_load = textArea.getText();	//更新修改前的檔案
+	        	}
+	            
+	        } 
+	        catch (Exception e) 
+	        {  
+	            // TODO Auto-generated catch block 
+	        	JOptionPane.showConfirmDialog(frame, "注意!\n檔案儲存時發生錯誤，請再試一次!", "錯誤", JOptionPane.OK_OPTION ,0);
 	            System.out.println("保存失敗");  
 	        }   
 	        
@@ -312,9 +413,9 @@ public class Final_Work  extends JFrame implements ActionListener
 	
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent arg0) 
+	{
 		// TODO Auto-generated method stub
-		
 	} 
 
 	
